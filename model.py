2b3fdb21a1281eed70886c6b1d97b842f76f0c7d
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-04-28 00:29:15 +0800

    4/28

--- model.py
+++ model.py
@@ -14,45 +14,48 @@ class CNN_01(nn.Module):
                 )
             return F
         self.cv1 = nn.Sequential(
-            conv_bn_relu(3, 8),
-            conv_bn_relu(8, 8),
+            conv_bn_relu(3, 64),
+            conv_bn_relu(64, 64),
             
             nn.MaxPool2d(2),
             
-            conv_bn_relu(8, 16),
-            conv_bn_relu(16, 16),
+            conv_bn_relu(64, 128),
+            conv_bn_relu(128, 128),
         )
         self.cv2 = nn.Sequential(
-            nn.Conv2d(16, 8, kernel_size=1),
-            nn.BatchNorm2d(8),
+            nn.Conv2d(128, 64, kernel_size=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),           
         )
 
         self.FC = nn.Sequential(
             nn.Flatten(),
+            nn.Linear(4096, 512),
+            nn.ReLU(),            
             nn.Linear(512, 128),
             nn.ReLU(),
             nn.Linear(128, 43),
+            nn.Softmax(-1)
         )
         self.tap_MP = nn.MaxPool2d(8)
         self.tap_AP = nn.AvgPool2d(8)
         self.MLP = nn.Sequential(
-            nn.Linear(8, 2),
+            nn.Linear(64, 4),
             nn.ReLU(),
-            nn.Linear(2, 8)
+            nn.Linear(4, 64)
             )    
-        self.sf_CBAM = nn.Softmax(-1)        
-        self.sf = nn.Softmax(-1)
+        self.sf = nn.Softmax(-1)        
     def forward(self, x):
         bz = x.size(0)
         y1 = self.cv1(x)
         y2 = self.cv2(y1)
+        print(y2.size())
         y2_MP = self.MLP(self.tap_MP(y2).view(bz, -1))
         y2_AP = self.MLP(self.tap_AP(y2).view(bz, -1))
         ch_atm = self.sf(y2_MP + y2_AP)
         y3 = y2 * ch_atm.view(bz, -1, 1, 1)
-        pred = self.sf(self.FC(y3))
+        pred = self.FC(y3)
         return pred, ch_atm
 
 #%% Test
