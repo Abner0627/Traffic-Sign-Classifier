6bb2e657ad6ebc0fa696b5d278eb4b13a41dc614
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-05-07 18:34:58 +0800

    5/7

--- README.md
+++ README.md
@@ -10,26 +10,45 @@
 
 ## 環境設定與套件安裝
 
-1. 使用環境：  
-    * Win 10 
-    * python 3.8.5
+**1. 使用環境**
+Win 10 
+python 3.8.5
 
-2. 進入該專案之資料夾
+**2. 進入該專案之資料夾**
 `cd /d [path/to/this/project]`
 
-3. 安裝所需套件 **(不含pytorch)**
+**3. 安裝所需套件 (不含pytorch)**
 `pip install -r requirements.txt`
 
-4. 至官網安裝Pytorch
+**4. 至官網安裝Pytorch**
 依官方下載頁面 ([Link](https://pytorch.org/get-started/locally/))，選擇與系統環境相符的版本下載即可，\
 以Win 10且具GPU (CUDA 11.1)為例：\
 `pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html`
 
-5. (選用) 安裝CUDA等所需套件
+**5. (選用) 安裝CUDA等所需套件**
 若要**使用tensorflow並調用GPU**時，則須依官網 ([Link](https://www.tensorflow.org/install/gpu?hl=zh-tw#software_requirements))指示安裝對應套件。
 
 ### 資料集分析
 
+**1. 各類別可視化**
+    本資料集共蒐集43類不同的德國交通號誌。\
+    從各類別隨機取樣40筆作圖可得下圖：
+    <img src=https://i.imgur.com/7uPPoyC.png>
+    由此可知，收集而成的資料有不同時間段或者光影環境所組成，\
+    因此能提升模型的強健性。\
+    \
+    如果要再進一步強化模型的學習能力，應再對資料作增強 (data augmentation)，\
+    如對圖片作旋轉任意角度或水平與垂直翻轉。
 
+**2. 各類別數量統計**
+    統計訓練集與驗證集各類別的數量，化成直方圖如下：
+    <img src=https://i.imgur.com/vRVy8tG.png>
+    <img src=https://i.imgur.com/xjjkOtl.png>
+    由此發現各類別的分佈相當不平均，若要增加分類的準確率，\
+    應對各類別有所增減使彼此數量相近，避免模型學習時過於偏向某一類別。
+
+3. 圖片數值分佈
+   隨機取樣三張圖片，統計其內的數值分佈可畫成以下直方圖：
+   <img src=https://i.imgur.com/KN6rK6Q.png>
 
 ### 程式碼註解
--- plot.py
+++ plot.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 import random
 
 #%% Args
-def config():
+def _config():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--plot", "-P", default=0, help="input the consumption data path")
+    parser.add_argument("-P", "--plot", default='0', help="plot")
     return parser.parse_args()
-args = config()
+
+
+def _norm(x):
+    mu = np.mean(x)
+    std = np.std(x)
+    nor = (x-mu)/std
+    return nor
 
 #%% Load
 dpath = './traffic-signs-data'
@@ -18,6 +24,7 @@ traF = 'train.p'
 valF = 'valid.p'
 tesF = 'test.p'
 sP = './img'
+args = _config()
 
 with open(os.path.join(dpath, traF), 'rb') as f:
     traD = pickle.load(f)
@@ -35,7 +42,7 @@ tra_label = traD['labels']
 val_label = valD['labels']
 tes_label = tesD['labels']
 
-if args.plot==0:
+if args.plot=='0':
     sF = 'img_' + str(args.plot) + '.png'
     ns = 40
     num_label = []
@@ -55,20 +62,95 @@ if args.plot==0:
     ax.imshow(np.vstack(total))
     plt.tight_layout()
     plt.axis('off')
-    plt.savefig(os.path.join(sP, sF))
+    plt.show()
+    # plt.savefig(os.path.join(sP, sF))
 
-elif args.plot==1:
+elif args.plot=='1':
     sF = 'img_' + str(args.plot) + '.png'
     num_label = []
     for i in range(43):
         idx = list(np.array(np.where(tra_label==i)).squeeze())
         num_label.append(len(idx))    
     num_label = np.array(num_label)
-
-    fig, ax = plt.subplots(1, 1, figsize=(15,15))
-    ax.bar(num_label)
+    x = [0, 7, 14, 21, 28, 35, 42]
+    fig, ax = plt.subplots(1, 1, figsize=(8,5))
+    ax.bar(np.arange(43), num_label)
+    ax.set_xticks(x)
+    ax.set_xticklabels(x)
+    plt.title('Training Data')
     plt.tight_layout()
     plt.savefig(os.path.join(sP, sF))    
-    plt.show()
+    # plt.show()
+
+elif args.plot=='2':
+    sF = 'img_' + str(args.plot) + '.png'
+    img0 = traRGB[1019,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+    img1 = traRGB[520,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+    img2 = traRGB[627,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+
+    img0 = np.reshape(img0, (32*32*3))
+    img1 = np.reshape(img1, (32*32*3))
+    img2 = np.reshape(img2, (32*32*3))
+
+    bins = np.linspace(0, 255, 256)
+
+    sl0, _ = np.histogram(img0, bins=bins)
+    sl1, _ = np.histogram(img1, bins=bins)
+    sl2, _ = np.histogram(img2, bins=bins)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8,5))
+    x = np.arange(255)
+    ax.bar(x, sl0, alpha=0.7, label='Sample 0')
+    ax.bar(x, sl1, alpha=0.7, label='Sample 1')
+    ax.bar(x, sl2, alpha=0.7, label='Sample 2')
+    ax.set_xlim(0, 120)
+    plt.legend(loc=1)
+    plt.tight_layout()
+    plt.savefig(os.path.join(sP, sF))    
+    # plt.show() 
+
+elif args.plot=='3':
+    sF = 'img_' + str(args.plot) + '.png'
+    img0 = traRGB[1019,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+    img1 = traRGB[520,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+    img2 = traRGB[627,:,:,:][np.newaxis,:,:,:].transpose(0,3,1,2)
+
+    img0 = _norm(np.reshape(img0, (32*32*3)))
+    img1 = _norm(np.reshape(img1, (32*32*3)))
+    img2 = _norm(np.reshape(img2, (32*32*3)))
+
+    bins = np.linspace(0, 1, 256)
+
+    sl0, _ = np.histogram(img0, bins=bins)
+    sl1, _ = np.histogram(img1, bins=bins)
+    sl2, _ = np.histogram(img2, bins=bins)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8,5))
+    x = np.linspace(0, 1, 255)
+    ax.bar(x, sl0, alpha=0.7, label='Sample 0', width=0.05)
+    ax.bar(x, sl1, alpha=0.7, label='Sample 1', width=0.05)
+    ax.bar(x, sl2, alpha=0.7, label='Sample 2', width=0.05)
+    ax.set_xlim(-1, 2)
+    plt.legend(loc=1)
+    plt.tight_layout()
+    plt.savefig(os.path.join(sP, sF))    
+    # plt.show()   
+
+elif args.plot=='4':
+    sF = 'img_' + str(args.plot) + '.png'
+    num_label = []
+    for i in range(43):
+        idx = list(np.array(np.where(val_label==i)).squeeze())
+        num_label.append(len(idx))    
+    num_label = np.array(num_label)
+    x = [0, 7, 14, 21, 28, 35, 42]
+    fig, ax = plt.subplots(1, 1, figsize=(8,5))
+    ax.bar(np.arange(43), num_label)
+    ax.set_xticks(x)
+    ax.set_xticklabels(x)
+    plt.title('Validation Data')
+    plt.tight_layout()
+    plt.savefig(os.path.join(sP, sF))    
+    # plt.show() 
 
 
